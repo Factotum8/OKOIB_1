24c8ab2a56fd342d294018d1b46bd2969e1a8c03
Your Name <you@example.com>
2016-11-18 14:52:21 +0300

    develop enum

--- wrfile.cpp
+++ wrfile.cpp
@@ -11,13 +11,108 @@ WRFile::WRFile(QString nameFile)
 
             QDomElement domElement = domDoc.documentElement();
 
-            tarverseNode(domElement);
+            count_ir = calculate_count_ir(domElement);
+
+            ir = new IR [count_ir];
+
+            traverseNode(domElement);
         }
         file.close();
     }
 }
 
-void WRFile::tarverseNode(const QDomNode& node)
+int WRFile::calculate_count_ir (const QDomNode& node)
 {
+    int count_ir=0;
 
+    QDomNode domNode = node.firstChild();
+    while(!domNode.isNull()) {
+        if(domNode.isElement()) {
+            QDomElement domElement = domNode.toElement();
+            if(!domElement.isNull()) {
+                if(domElement.tagName() == "ir") {
+
+                    count_ir = domElement.attribute("number", "").toInt();
+
+                }
+
+            }
+        }
+        calculate_count_ir(domNode);
+        domNode = domNode.nextSibling();
+    }
+
+    return count_ir;
+}
+
+void WRFile::traverseNode(const QDomNode& node)
+{
+    int i=0;
+
+    bool* flag= new bool;
+
+    QMetaEnum metaEnum;
+
+    QDomNode domNode = node.firstChild();
+
+    while(!domNode.isNull()) {
+
+        if(domNode.isElement()) {
+
+            QDomElement domElement = domNode.toElement();
+
+            if(!domElement.isNull())
+
+            { // tag = metaEnum.keyToValue( domElement.tagName().toStdString().c_str(), flag );
+                //tag =  domElement.tagName().toStdString().c_str();
+//                tag = name;
+
+//                switch (tag) {
+//                case info_resource:
+//                    i = domElement.attribute("number", "").toInt();
+//                    break;
+
+//                case name:
+//                    ir[i].set_name(domElement.text());
+//                    break;
+
+//                case first_year:
+
+//                    ir[i].set_first_year(QDate (domElement.text().toInt(),1,1));
+//                    break;
+
+//                case 7:
+
+//                    break;
+//                default:
+//                    break;
+//                }
+            }
+        }
+
+
+        traverseNode(domNode);
+        domNode = domNode.nextSibling();
+    }
+}
+
+
+const QString WRFile::tag() const
+{
+    const QMetaObject &mo = WRFile::staticMetaObject;
+    int index = mo.indexOfEnumerator("Type");
+    QMetaEnum metaEnum = mo.enumerator(index);
+
+    return metaEnum.valueToKey(tag);
+}
+
+void WRFile::setType(const QString &tag)
+{
+    const QMetaObject &mo = WRFile::staticMetaObject;
+
+    int index = mo.indexOfEnumerator("Tag");
+    QMetaEnum metaEnum = mo.enumerator(index);
+    int value = metaEnum.keyToValue(qPrintable(tag));
+
+    tag = static_cast<Tag>(value);
 }
--- wrfile.h
+++ wrfile.h
@@ -2,13 +2,36 @@
 #define WRFILE_H
 
 #include <QtXml>
+#include <QMetaEnum>
+#include <QDebug>
+#include <QObject>
+
+
+#include "global.h"
+#include "struct.h"
+
+
+extern IR *ir;
+extern int count_ir;
+extern cost_index* c_index;
+extern int count_cost_index;
 
 class WRFile
 {
+    Q_GADGET //https://www.christian-gmeiner.info/2015/05/qmetaenum-serializing-c-enums/
+    Q_ENUMS(Tag)
+
 public:
     WRFile(QString nameFile);
 private:
-    void tarverseNode(const QDomNode& node);
+    enum Tag tag ;
+    const QString tag();
+
+    int calculate_count_ir (const QDomNode& node);
+    void traverseNode(const QDomNode& node);
+
+    void setType(enum  Tag tag);
+    void setType(const QString &tag);
 };
 
 #endif // WRFILE_H
